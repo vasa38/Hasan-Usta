b12ee81c8fb0b5c61a864a1dbb2dde70055ed175
ayberkvaroll <ayberkvarol07@gmail.com>
2023-12-03 19:08:36 +0300

    up

--- src/app/hero.tsx
+++ src/app/hero.tsx
@@ -11,13 +11,17 @@ function Hero() {
       className="!flex h-[55vh] w-full items-center justify-between px-10"
     >
       <Image width={1200} height={1200} src="/image/logo.jpeg" alt="bg-img" />
-      <Image
-        width={500}
-        height={500}
-        src="/image/image8.jpeg"
-        alt="bg-img"
-        className="absolute inset-0 ml-auto w-[920px] h-[780px] rounded-bl-[100px] object-cover object-center"
-      />
+      {isMobile ? (
+        <Image
+          width={500}
+          height={500}
+          src="/image/image8.jpeg"
+          alt="bg-img"
+          className="absolute inset-0 ml-auto w-[920px] h-[780px] rounded-bl-[100px] object-cover object-center"
+        />
+      ) : (
+        <></>
+      )}
       <div className="container mx-auto mt-28">
         <div className="grid grid-cols-12 text-center lg:text-left">
           <Card className="col-span-full rounded-xl border border-white bg-white/90 py-10 p-8 shadow-lg shadow-black/10 backdrop-blur-sm backdrop-saturate-200 xl:col-span-7">
