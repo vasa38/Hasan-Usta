7e1d6bbcbd75c95f5ebe7f5fc8b438fce8cdcc7f
ayberkvaroll <ayberkvarol07@gmail.com>
2023-12-03 19:06:40 +0300

    a

--- src/app/hero.tsx
+++ src/app/hero.tsx
@@ -10,9 +10,10 @@ function Hero() {
       style={{ marginTop: isMobile ? 1000 : 0 }}
       className="!flex h-[55vh] w-full items-center justify-between px-10"
     >
+      <Image width={1200} height={1200} src="/image/logo.jpeg" alt="bg-img" />
       <Image
-        width={1200}
-        height={1200}
+        width={500}
+        height={500}
         src="/image/image8.jpeg"
         alt="bg-img"
         className="absolute inset-0 ml-auto w-[920px] h-[780px] rounded-bl-[100px] object-cover object-center"
