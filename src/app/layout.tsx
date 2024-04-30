e24300bc36067f6328c9850f86a2bfa7f80129e6
Mehmet Kerim Sarğın <89380376+kerimsarginnn@users.noreply.github.com>
2024-04-30 10:42:31 +0300

    w

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,16 +27,9 @@ export default function RootLayout({
           data-site="www.tesisatcihasanusta.com"
           src="https://api.nepcha.com/js/nepcha-analytics.js"
         ></script>
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-5CMBPB4C');</script>
         <link rel="shortcut icon" href="/favicon.png" type="image/png" />
       </head>
       <body className={roboto.className}>
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5CMBPB4C"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <Layout>
           {children}
           <FixedPlugin />
